1b015ddd2df06d77f9287cefed2be4a0478433d4
MohammedAlmasabi <133659122+MohMusaed@users.noreply.github.com>
2024-09-30 21:06:28 +0300

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from transformers import pipeline
+import streamlit as st
+from PIL import Image
 
 pipe = pipeline("image-classification", model="julien-c/hotdog-not-hotdog")
 
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    img = image.open(uploaded_file)
+    img = Image.open(uploaded_file)
     st.image(img)
-    predictions = pipe(imge)
+    predictions = pipe(img)
     st.write(predictions)
-
-img = Image.open(uploaded_file)
-
-predictions = pipe(img)
